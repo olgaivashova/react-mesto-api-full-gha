27ccecb09b9a34c6382035a88ca81d4c6201cefd
Olga Ivashova <ivashova@list.ru>
2023-09-01 13:02:57 +0300

    cors version 1

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const app = express();
+const cors = require("cors");
+
 const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mydb" } = process.env;
 const bodyParser = require("body-parser");
 
-const app = express();
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-const cors = require("cors");
 const helmet = require("helmet");
 const { errors } = require("celebrate");
 const routerUsers = require("./routes/users");
@@ -21,6 +20,7 @@ const auth = require("./middlewares/auth");
 const NotFoundError = require("./errors/notFoundError");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
+app.use(cors());
 mongoose
   .connect(DB_URL, {
     useNewUrlParser: true,
@@ -29,11 +29,17 @@ mongoose
     // eslint-disable-next-line no-console
     console.log(123456);
   });
-app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(requestLogger); // подключаем логгер запросов до роутов
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 app.use("/signup", routerSignup);
 app.use("/signin", routerLogin);
 app.use(auth);
