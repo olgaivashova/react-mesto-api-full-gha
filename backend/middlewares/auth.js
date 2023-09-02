d15f476bc22e27d3d4ad95ff11849690c3f68996
Olga Ivashova <ivashova@list.ru>
2023-09-02 14:07:31 +0300

    add .env again

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-// const { SECRET_KEY = "hello mesto" } = process.env;
+const { SECRET_JWT = "hello mesto" } = process.env;
 
 const UnauthorizedError = require("../errors/unauthorizedError");
 
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
 
   try {
     // попытаемся верифицировать токен
-    payload = jwt.verify(token, "some-secret-key");
+    payload = jwt.verify(token, SECRET_JWT);
   } catch (err) {
     // отправим ошибку, если не получилось
     throw new UnauthorizedError("Необходима авторизация");
